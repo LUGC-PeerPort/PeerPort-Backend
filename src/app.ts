1451cc9c9d11c0809ded9c9e6db5b31fc5bea4e2
CommanderKV <kylervisser@gmail.com>
2025-09-11 18:26:49 -0400

    Added checks for enviroment vairables and added server start message

--- src/app.ts
+++ src/app.ts
@@ -6,12 +6,22 @@ import { UserController } from "./Controllers/UserController.js";
 const app = express();
 app.use(express.json());
 
+// Check if the enviroment variables are set
+if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASSWORD || !process.env.DB_NAME) {
+    console.error("Database environment variables are not set.");
+    process.exit(1);
+}
+
 AppDataSource.initialize().then(() => {
     const userRepo = AppDataSource.getRepository(User);
     const userController = new UserController(userRepo);
 
     app.get("/users", (req, res) => userController.getAllUsers(req, res));
     app.post("/users", (req, res) => userController.create(req, res));
+})
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000 at http://localhost:3000/");
 })
 
 export default app;
