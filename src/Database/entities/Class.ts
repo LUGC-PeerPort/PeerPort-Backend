261285a3903aff16885e403a5d94a73288756522
CommanderKV <kylervisser@gmail.com>
2025-09-12 17:08:16 -0400

    Updated comment

--- src/Database/entities/Class.ts
+++ src/Database/entities/Class.ts
@@ -2,7 +2,7 @@ import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from "typeorm";
 import { UsersToClasses } from "./UsersToClasses.js";
 
 /**
- *
+ * Used to represent a class/course the the user can be enrolled in.
  */
 @Entity("Classes")
 export class Class {
