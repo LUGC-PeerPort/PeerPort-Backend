d1ead18edb88292f2b2dab4a9c137a213030c6e4
CommanderKV <kylervisser@gmail.com>
2025-09-13 17:05:55 -0400

    Passed course repo to usercontroller

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import { User } from "./Database/entities/User.js";
 import express from "express";
 import { AppDataSource } from "./Database/DB.js";
 import { UserController } from "./Controllers/UserController.js";
+import { Class } from "./Database/entities/Class.js";
 
 const app = express();
 app.use(express.json());
@@ -14,7 +15,8 @@ if (!process.env.DB_HOST || !process.env.DB_USER || !process.env.DB_PASSWORD || 
 
 AppDataSource.initialize().then(() => {
     const userRepo = AppDataSource.getRepository(User);
-    const userController = new UserController(userRepo);
+    const courseRepo = AppDataSource.getRepository(Class);
+    const userController = new UserController(userRepo, courseRepo);
 
     app.get("/users", (req, res) => userController.getAllUsers(req, res));
     app.post("/users", (req, res) => userController.create(req, res));
