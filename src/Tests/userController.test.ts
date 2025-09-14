9ab3cebbb6866e3532003e08a929087c064dbcc3
CommanderKV <kylervisser@gmail.com>
2025-09-13 22:24:42 -0400

    Added most creation testing for user controller

--- src/Tests/userController.test.ts
+++ src/Tests/userController.test.ts
@@ -20,11 +20,111 @@ describe("UserController test:", () => {
         it("Should not create a user with an invalid structure", async () => {
             const req: any = {
                 body: {
-                    name: "Tesrt User",
+                    namee: "Test User",
                     email: "",
-
                 }
             };
+
+            const res: any = {};
+            res.status = jest.fn().mockReturnValue(res);
+            res.json = jest.fn().mockReturnValue(res);
+            await controller.create(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ message: "Invalid user structure" });
+        });
+
+        it("Should not create a user with less than 2 chacracters in the name", async () => {
+            const req: any = {
+                body: {
+                    name: "T",
+                    email: "testuser@example.com",
+                    password: "securepassword",
+                    profilePictureURL: "",
+                    idNumber: "123456789smth",
+                }
+            };
+
+            const res: any = {};
+            res.status = jest.fn().mockReturnValue(res);
+            res.json = jest.fn().mockReturnValue(res);
+            await controller.create(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ message: "Invalid user structure" });
+        });
+
+        it("Should not create a user with an invalid email", async () => {
+            const req: any = {
+                body: {
+                    name: "Test User",
+                    email: "notanemail",
+                    password: "securepassword",
+                    profilePictureURL: "",
+                    idNumber: "123456789smth",
+                }
+            };
+
+            const res: any = {};
+            res.status = jest.fn().mockReturnValue(res);
+            res.json = jest.fn().mockReturnValue(res);
+            await controller.create(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ message: "Invalid user structure" });
+        });
+
+        it("Should not create a user with an empty email", async () => {
+            const req: any = {
+                body: {
+                    name: "Test User",
+                    email: "",
+                    password: "securepassword",
+                    profilePictureURL: "",
+                    idNumber: "123456789smth",
+                }
+            };
+
+            const res: any = {};
+            res.status = jest.fn().mockReturnValue(res);
+            res.json = jest.fn().mockReturnValue(res);
+            await controller.create(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ message: "Invalid user structure" });
+        });
+
+        it("Should not create a user with an empty password", async () => {
+            const req: any = {
+                body: {
+                    name: "Test User",
+                    email: "testuser@example.com",
+                    password: "",
+                    profilePictureURL: "",
+                    idNumber: "123456789smth",
+                }
+            };
+
+            const res: any = {};
+            res.status = jest.fn().mockReturnValue(res);
+            res.json = jest.fn().mockReturnValue(res);
+            await controller.create(req, res);
+
+            expect(res.status).toHaveBeenCalledWith(400);
+            expect(res.json).toHaveBeenCalledWith({ message: "Invalid user structure" });
+        });
+
+        it("Should not create a user with an empty id number", async () => {
+            const req: any = {
+                body: {
+                    name: "Test User",
+                    email: "testuser@example.com",
+                    password: "securepassword",
+                    profilePictureURL: "",
+                    idNumber: "",
+                }
+            };
+
             const res: any = {};
             res.status = jest.fn().mockReturnValue(res);
             res.json = jest.fn().mockReturnValue(res);
@@ -35,6 +135,10 @@ describe("UserController test:", () => {
         });
 
         it("Should create a user with valid data", async () => {
+            await TestDataSource.destroy();
+            await TestDataSource.initialize();
+            controller = new UserController(TestDataSource);
+
             const req: any = {
                 body: {
                     name: "Test User",
