bc352ac8fc778ab197b86460ed8ef05c17fb928a
CommanderKV <kylervisser@gmail.com>
2025-09-30 18:44:09 -0400

    Added all the entities to the tests data source

--- src/Tests/test-data-source.ts
+++ src/Tests/test-data-source.ts
@@ -1,8 +1,15 @@
 import { DataSource } from "typeorm";
 import { User } from "../Database/entities/User.js";
 import { Role } from "../Database/entities/Role.js";
+import { Files } from "../Database/entities/Files.js";
 import { Course } from "../Database/entities/Course.js";
+import { Content } from "../Database/entities/Content.js";
+import { Assignments } from "../Database/entities/Assignments.js";
 import { UsersToCourses } from "../Database/entities/UsersToCourses.js";
+import { ContentToFiles } from "../Database/entities/ContentToFiles.js";
+import { AssignmentToFiles } from "../Database/entities/AssignmentToFiles.js";
+import { AssignmentSubmissions } from "../Database/entities/AssignmentSubmissions.js";
+import { AssignmentSubmissionToFiles } from "../Database/entities/AssignmentSubmissionToFiles.js";
 
 export const TestDataSource = new DataSource({
     type: "sqlite",
@@ -12,7 +19,14 @@ export const TestDataSource = new DataSource({
     entities: [
         User, 
         Role, 
+        Files,
         Course,
-        UsersToCourses
+        Content,
+        Assignments,
+        UsersToCourses,
+        ContentToFiles,
+        AssignmentToFiles,
+        AssignmentSubmissions,
+        AssignmentSubmissionToFiles,
     ],
 });
