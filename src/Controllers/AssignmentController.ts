ac105be7a0f6a5c7dad46d879380bb1cb4268bf6
CommanderKV <kylervisser@gmail.com>
2025-09-30 16:29:05 -0400

    Add methods for assignment management in AssignmentController

--- src/Controllers/AssignmentController.ts
+++ src/Controllers/AssignmentController.ts
@@ -1,5 +1,6 @@
 import type { DataSource, Repository } from "typeorm";
 import { Assignments } from "../Database/entities/Assignments";
+import type { Request, Response } from "express";
 
 /**
  * Used to manage assignments.
@@ -14,4 +15,18 @@ export class AssignmentController {
     constructor(dataSource: DataSource) {
         this.assignmentRepo = dataSource.getRepository(Assignments);
     }
+
+    async getAllAssignments(req: Request, res: Response): Promise<void> {}
+
+    async createAssignment(req: Request, res: Response): Promise<void> {}
+
+    async getAssignment(req: Request, res: Response): Promise<void> {}
+
+    async updateAssignment(req: Request, res: Response): Promise<void> {}
+
+    async deleteAssignment(req: Request, res: Response): Promise<void> {}
+
+    async getSubmissionsForAssignment(req: Request, res: Response): Promise<void> {}
+
+    async createSubmissionForAssignment(req: Request, res: Response): Promise<void> {}
 }
