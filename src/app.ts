aa154cfc70172f075465ea43ec50fed3a1a2823d
AidenLortie <146017618+AidenLortie@users.noreply.github.com>
2025-10-19 18:36:26 -0400

    Cors issue?

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,8 @@ app.use(cors({
     origin: process.env.CLIENT_URL,
     methods: "GET,POST,PUT,DELETE,HEAD,OPTIONS",
     credentials: true,
-    allowedHeaders: "Content-Type,Authorization"
+    allowedHeaders: "Content-Type,Authorization",
+    cookie: { secure: false }
 }));
 
 
