74c9ede41f6cdb02ba020f15ee1d05510fe288d8
CommanderKV <kylervisser@gmail.com>
2025-10-19 17:12:22 -0400

    Fixed DB so the openAPI works

--- src/Database/DB.ts
+++ src/Database/DB.ts
@@ -1,5 +1,17 @@
 import "reflect-metadata";
 import { DataSource } from "typeorm";
+import { User } from "../Database/entities/User.js";
+import { Role } from "../Database/entities/Role.js";
+import { Files } from "../Database/entities/Files.js";
+import { Course } from "../Database/entities/Course.js";
+import { Content } from "../Database/entities/Content.js";
+import { Assignments } from "../Database/entities/Assignments.js";
+import { UsersToCourses } from "../Database/entities/UsersToCourses.js";
+import { ContentToFiles } from "../Database/entities/ContentToFiles.js";
+import { AssignmentToFiles } from "../Database/entities/AssignmentToFiles.js";
+import { AssignmentSubmissions } from "../Database/entities/AssignmentSubmissions.js";
+import { AssignmentSubmissionToFiles } from "../Database/entities/AssignmentSubmissionToFiles.js";
+
 
 export const AppDataSource = new DataSource({
     type: "postgres",
@@ -9,7 +21,18 @@ export const AppDataSource = new DataSource({
     database: process.env.DB_NAME,
 
     // Using the models
-    entities: ["./entities/*.js"],
+    entities: [
+        User, 
+        Role, 
+        Files,
+        Course,
+        Content,
+        Assignments,
+        UsersToCourses,
+        ContentToFiles,
+        AssignmentToFiles,
+        AssignmentSubmissions,
+        AssignmentSubmissionToFiles,
+    ],
     synchronize: true,
-
 });
