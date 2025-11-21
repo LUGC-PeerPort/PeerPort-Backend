1155d2b31ef6dce2251c7abf48483e8e16bb1425
allortie <24lolks@gmail.com>
2025-11-21 10:07:55 -0500

    Removed console.log

--- src/Auth/GoogleStrategy.ts
+++ src/Auth/GoogleStrategy.ts
@@ -119,7 +119,6 @@ export const GoogleStrategySetup = (app: express.Express, AppDataSource:DataSour
                 userRole = roleUser;
                 user.role = roleUser;
                 await AppDataSource.getRepository(User).save(user);
-                console.log(user);
             } else {
                 console.error("User not found");
                 res.json({error: "User Role not found"}).status(500);
