7ffa6020244809e1f2ea79cf659b4d5d58fcbee2
CommanderKV <kylervisser@gmail.com>
2025-11-22 05:24:13 -0500

    Fixed duplicate tool in mock

--- tests/__mocks__/toolsMock.ts
+++ tests/__mocks__/toolsMock.ts
@@ -46,18 +46,6 @@ export async function checkIfUserRelatedToCourse(req: Request, res: Response, us
  * @param userRepo - The user DB
  * @returns Whether the user is allowed to edit the user profile
  */
-export async function checkIfUserEditUser(req: Request, res: Response, userRepo: Repository<User>): Promise<boolean> {
-    return true;
-}
-
-
-/**
- * Checks if the user is allowed to edit/get from the user profile
- * @param req - The Request object
- * @param res - The Response object
- * @param userRepo - The user DB
- * @returns Whether the user is allowed to edit the user profile
- */
 export async function checkIfUserRelatedToUser(req: Request, res: Response, userRepo: Repository<User>): Promise<boolean> {
     return true;
 }
