722160173a86079f3a8e39604b7fb3539a173982
CommanderKV <kylervisser@gmail.com>
2025-11-22 03:19:15 -0500

    Added security

--- src/Controllers/SubmissionController.ts
+++ src/Controllers/SubmissionController.ts
@@ -1,13 +1,17 @@
 import type { DataSource, Repository } from "typeorm";
 import type { Request, Response } from "express";
 import { AssignmentSubmissions } from "../Database/entities/AssignmentSubmissions.js";
-import { checkUUID } from "./Tools.js";
+import { checkIfUserRelatedToUser, checkUUID } from "./Tools.js";
+import { UsersToCourses } from "../Database/entities/UsersToCourses.js";
+import { User } from "../Database/entities/User.js";
 
 /**
  * Used to manage assignment submissions.
  */
 export class SubmissionController {
     private submissionRepo: Repository<AssignmentSubmissions>;
+    private userRepo: Repository<User>;
+    private userToCourseRepo: Repository<UsersToCourses>;
     
     /**
      * Constructor for SubmissionController.
@@ -15,6 +19,8 @@ export class SubmissionController {
      * */
     constructor(dataSource: DataSource) {
         this.submissionRepo = dataSource.getRepository(AssignmentSubmissions);
+        this.userRepo = dataSource.getRepository(User);
+        this.userToCourseRepo = dataSource.getRepository(UsersToCourses);
     }
 
     /**
@@ -62,6 +68,14 @@ export class SubmissionController {
                 res.status(404).json({ message: "Submission not found" });
                 return;
             }
+
+            // Check that the user is apart of the course
+            const tempReq = req;
+            tempReq.params = { userId: submission.user.userId };
+            if (!await checkIfUserRelatedToUser(tempReq, res, this.userRepo)) {
+                return;
+            }
+
             res.status(200).json({
                 comment: submission.comment,
                 timeSubmitted: submission.timeSubmitted,
